699082f9b44c9761a6cb0fb30a72579e1d1d127d
Enrique Benitez <enriquebenitezjr@gmail.com>
2019-08-19 10:17:42 -0500

    feat: query projects and create slugs

--- gatsby-theme-personal/gatsby-node.js
+++ gatsby-theme-personal/gatsby-node.js
@@ -8,3 +8,35 @@ exports.onPreBootstrap = ({ reporter }) => {
     fs.mkdirSync(contentPath)
   }
 }
+
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Project implements Node @dontInfer {
+      id: ID!
+      name: String!
+      description: String!
+      type: String
+      release: Int!
+      url: String!
+      status: String!
+      slug: String!
+    }
+  `)
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = '/'
+
+  const slugify = str => {
+    const slug = str.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '')
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, '/')
+  }
+
+  createResolvers({
+    Project: {
+      slug: {
+        resolve: source => slugify(source.name)
+      }
+    }
+  })
+}
